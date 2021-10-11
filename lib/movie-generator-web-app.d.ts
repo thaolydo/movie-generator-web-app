32816cb350069b5a98293723e3dbe0a244eecd34
Ly Do <lydo1797@gmail.com>
2021-10-11 16:01:17 -0700

    Remove *d.ts files.

--- lib/movie-generator-web-app.d.ts
+++
@@ -1,5 +0,0 @@
-import { Construct, Stage, StageProps } from '@aws-cdk/core';
-import { StageConfig } from './stage-config';
-export declare class MovieGeneratorWebApp extends Stage {
-    constructor(scope: Construct, id: string, stageConfigs: StageConfig, props?: StageProps);
-}
--- lib/web-app-deployment-stack.d.ts
+++
@@ -1,6 +0,0 @@
-import { CfnOutput, Construct, Stack, StackProps } from "@aws-cdk/core";
-import { StageConfig } from './stage-config';
-export declare class WebAppDeploymentStack extends Stack {
-    readonly webDistributionUrlOutput: CfnOutput;
-    constructor(scope: Construct, id: string, stageConfigs: StageConfig, props?: StackProps);
-}
