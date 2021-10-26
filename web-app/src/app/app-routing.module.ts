4ff5c81b7084ed1142e9b2cf251ef713ea733e54
Lennox Scott <chrislennoxs@hotmail.com>
2021-10-26 11:55:00 -0400

    ShareMoviePage Created
    The share movie page is created with binding implemented.

--- web-app/src/app/app-routing.module.ts
+++ web-app/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './componenets/header/header.component';
 import { LandingPageComponent } from './Pages/landing-page/landing-page.component';
 import { LoginPageComponent } from './Pages/login-page/login-page.component';
+import { ShareMoviePageComponent } from './Pages/share-movie-page/share-movie-page.component';
 import { SignupPageComponent } from './Pages/signup-page/signup-page.component';
 
 const routes: Routes = [
   {path: "landing-page", component: LandingPageComponent},
   {path: "login-page", component: LoginPageComponent},
-  {path: "signup-page", component: SignupPageComponent}
+  {path: "signup-page", component: SignupPageComponent},
+  {path: "share-movie-page", component: ShareMoviePageComponent}
 
 ];
 
