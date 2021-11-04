3519b2492ab13db90731094e5bd820a6f601dd14
Lennox Scott <chrislennoxs@hotmail.com>
2021-11-04 13:19:57 -0400

    First 3 modals created
    I was able to create three new modals , for the appearance, account details, and recommendations

--- web-app/src/app/Pages/settings-page/settings-page.component.ts
+++ web-app/src/app/Pages/settings-page/settings-page.component.ts
@@ -3,17 +3,137 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-settings-page',
   templateUrl: './settings-page.component.html',
-  styleUrls: ['./settings-page.component.scss']
+  styleUrls: ['./settings-page.component.scss'],
 })
 export class SettingsPageComponent implements OnInit {
+   
 
-  constructor() { }
+  settingsModalVisible: string = 'block';
+  clickModalVisible: string = 'none'
+  AccountDetailsModalVisible: string = 'none';
+  RecommendationModalVisible: string = 'none';
+  appearanceModalVisible: string = 'none';
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
-  alertMe = () =>{
-    alert("I work")
-  }
+  ngOnInit(): void {}
 
+  alertMe = () => {
+    alert('I work');
+  };
+
+  showSettingsModal = () => {
+    // hide the settings modal and show the options modal
+  };
+
+  showAccountDetailsModal = () => {
+    let accountDetailsPopup = document.getElementById('accountDetailsPopup');
+    let mainSettingsModal = document.getElementById('settingsPageModal');
+    let clickModal = document.getElementById('clickModal');
+
+    if(this.settingsModalVisible == 'none'){// shows the settings modal
+      this.settingsModalVisible = 'block'
+      if (mainSettingsModal)
+      (mainSettingsModal as HTMLFormElement).style.display = 'block';
+    }else if (this.settingsModalVisible == 'block'){// hides the settings modal
+      this.settingsModalVisible = 'none'
+      if (mainSettingsModal)
+      (mainSettingsModal as HTMLFormElement).style.display = 'none';
+    }
+
+    if(this.clickModalVisible == 'none'){// shows the click modal
+      this.clickModalVisible = 'block'
+      if (clickModal)
+      (clickModal as HTMLFormElement).style.display = 'block';
+    }else if (this.clickModalVisible == 'block'){// hides the click modal
+      this.clickModalVisible = 'none'
+      if (clickModal)
+      (clickModal as HTMLFormElement).style.display = 'none';
+    }
+
+    if(this.AccountDetailsModalVisible == 'none'){// shows the appearance modal
+      this.AccountDetailsModalVisible = 'flex'
+      if (accountDetailsPopup)
+      (accountDetailsPopup as HTMLFormElement).style.display = 'flex';
+    }else if (this.AccountDetailsModalVisible == 'flex'){// hides the appearance modal
+      this.AccountDetailsModalVisible = 'none'
+      if (accountDetailsPopup)
+      (accountDetailsPopup as HTMLFormElement).style.display = 'none';
+    }
+  };
+
+  showRecommendationModal = () => {
+    let recommendationsPopup = document.getElementById('recommendatioPreferencesPopup');
+    let mainSettingsModal = document.getElementById('settingsPageModal');
+    let clickModal = document.getElementById('clickModal');
+
+    if(this.settingsModalVisible == 'none'){// shows the settings modal
+      this.settingsModalVisible = 'block'
+      if (mainSettingsModal)
+      (mainSettingsModal as HTMLFormElement).style.display = 'block';
+    }else if (this.settingsModalVisible == 'block'){// hides the settings modal
+      this.settingsModalVisible = 'none'
+      if (mainSettingsModal)
+      (mainSettingsModal as HTMLFormElement).style.display = 'none';
+    }
+
+    if(this.clickModalVisible == 'none'){// shows the click modal
+      this.clickModalVisible = 'block'
+      if (clickModal)
+      (clickModal as HTMLFormElement).style.display = 'block';
+    }else if (this.clickModalVisible == 'block'){// hides the click modal
+      this.clickModalVisible = 'none'
+      if (clickModal)
+      (clickModal as HTMLFormElement).style.display = 'none';
+    }
+
+    if(this.RecommendationModalVisible == 'none'){// shows the appearance modal
+      this.RecommendationModalVisible = 'flex'
+      if (recommendationsPopup)
+      (recommendationsPopup as HTMLFormElement).style.display = 'flex';
+    }else if (this.RecommendationModalVisible == 'flex'){// hides the appearance modal
+      this.RecommendationModalVisible = 'none'
+      if (recommendationsPopup)
+      (recommendationsPopup as HTMLFormElement).style.display = 'none';
+    }
+  };
+
+  showAppearanceModal = () => {
+    let appearancePopUp = document.getElementById('appearancePopup');
+    let mainSettingsModal = document.getElementById('settingsPageModal');
+    let clickModal = document.getElementById('clickModal');
+
+    if(this.settingsModalVisible == 'none'){// shows the settings modal
+      this.settingsModalVisible = 'block'
+      if (mainSettingsModal)
+      (mainSettingsModal as HTMLFormElement).style.display = 'block';
+    }else if (this.settingsModalVisible == 'block'){// hides the settings modal
+      this.settingsModalVisible = 'none'
+      if (mainSettingsModal)
+      (mainSettingsModal as HTMLFormElement).style.display = 'none';
+    }
+
+    if(this.clickModalVisible == 'none'){// shows the click modal
+      this.clickModalVisible = 'block'
+      if (clickModal)
+      (clickModal as HTMLFormElement).style.display = 'block';
+    }else if (this.clickModalVisible == 'block'){// hides the click modal
+      this.clickModalVisible = 'none'
+      if (clickModal)
+      (clickModal as HTMLFormElement).style.display = 'none';
+    }
+
+    if(this.appearanceModalVisible == 'none'){// shows the appearance modal
+      this.appearanceModalVisible = 'flex'
+      if (appearancePopUp)
+      (appearancePopUp as HTMLFormElement).style.display = 'flex';
+    }else if (this.appearanceModalVisible == 'flex'){// hides the appearance modal
+      this.appearanceModalVisible = 'none'
+      if (appearancePopUp)
+      (appearancePopUp as HTMLFormElement).style.display = 'none';
+    }
+
+    
+    
+  };
 }
