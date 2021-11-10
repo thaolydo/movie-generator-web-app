f7bed6d3e0fb2c3c33cd0de430c0f110512afa0e
Lennox Scott <chrislennoxs@hotmail.com>
2021-11-10 14:05:41 -0500

    Created sent modal

--- web-app/src/app/Pages/share-movie-page/share-movie-page.component.ts
+++ web-app/src/app/Pages/share-movie-page/share-movie-page.component.ts
@@ -9,7 +9,43 @@ export class ShareMoviePageComponent implements OnInit {
   receiverName: string = '';
   receiverEmail: string = '';
 
+  settingsModalVisible: string = 'flex';
+  clickModalVisible: string = 'none';
+  shareMovieSentModalVisible: string = 'none';
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  showShareMovieSentModal = () => {
+    let shareMovieSentPopup = document.getElementById('shareMovieSentOuterModal');
+    let mainShareModal = document.getElementById('shareMoviePageModal');
+    let clickModal = document.getElementById('clickModal');
+
+    if (this.settingsModalVisible == 'none') {
+      // shows the settings modal
+      this.settingsModalVisible = 'flex';
+      if (mainShareModal)
+        (mainShareModal as HTMLFormElement).style.display = 'flex';
+    } else if (this.settingsModalVisible == 'flex') {
+      // hides the settings modal
+      this.settingsModalVisible = 'none';
+      if (mainShareModal)
+        (mainShareModal as HTMLFormElement).style.display = 'none';
+    }
+
+    
+
+    if (this.shareMovieSentModalVisible == 'none') {
+      // shows the appearance modal
+      this.shareMovieSentModalVisible = 'flex';
+      if (shareMovieSentPopup)
+        (shareMovieSentPopup as HTMLFormElement).style.display = 'flex';
+    } else if (this.shareMovieSentModalVisible == 'flex') {
+      // hides the appearance modal
+      this.shareMovieSentModalVisible = 'none';
+      if (shareMovieSentPopup)
+        (shareMovieSentPopup as HTMLFormElement).style.display = 'none';
+    }
+  };
 }
